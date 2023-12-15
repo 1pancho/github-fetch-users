e9ba3367aff6e2f0e79072f681f1800543feb8b3
1pancho <medovmail@gmail.com>
2023-12-15 14:18:15 +0300

    fix: minor typos in code

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FC, useEffect, useState } from 'react'
 import styles from './Pagination.module.css'
-import usePagination from '../../hooks/usePagination'
 
 type Props = {
   reposPerPage: number
@@ -10,14 +9,24 @@ type Props = {
 }
 
 const Pagination: FC<Props> = ({ reposPerPage, totalRepos, paginate }) => {
-  const pageNumbers = usePagination(totalRepos, reposPerPage);
-  if (!pageNumbers) {
-    return null;
-  }
+  const [pageNumbers, setPageNumbers] = useState<number[]>([]);
+
+  useEffect(() => {
+    const calculatePageNumbers = () => {
+      const numbers = [];
+      for (let i = 1; i <= Math.ceil(totalRepos / reposPerPage); i++) {
+        numbers.push(i);
+      }
+      setPageNumbers(numbers);
+    };
+
+    calculatePageNumbers();
+  }, [reposPerPage, totalRepos]);
+
   return (
     <div className={styles.reposContainerPagination}>
       <ul className="pagination">
-        {pageNumbers?.map((number) => (
+        {pageNumbers.map((number) => (
           <li className="page-item" key={number}>
             <a href="!#" className="page-link" onClick={() => paginate(number)}>
               {number}
